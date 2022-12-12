479772e3eafe984e92a4d8615a89a9bacc4ab3ee
ivoxavier <ivofernandes12@gmail.com>
2022-12-12 21:03:53 +0000

    Create kaltracker-main.sh

---
+++ kaltracker-main.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "Current Tag Version:"
+current_tag= git describe --tags --abbrev=0
+case $1 in
+ --desktop)
+ clickable desktop
+ ;;
+ --device)
+ clickable
+ ;;
+ --build-all)
+ clickable build -a arm64
+ clickable build -a armhf
+ ;;
+ --add-tag)
+ echo -n "New Tag: "
+ read new_tag
+ echo $new_tag
+ git tag $new_tag
+ esac
