6410253f3e737463ee5ca1d50d2d06c7a6c047b7
ivoxavier <ivofernandes12@gmail.com>
2022-12-24 19:29:11 +0000

    Remove page_stack pops $ push

--- js/ControlRecommendedCalories.js
+++ js/ControlRecommendedCalories.js
@@ -14,19 +14,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-function confrim(){
-  ////stores plan type
-  app_settings.plan_type = root.type_goal
-  
-  page_stack.pop(set_height_page)
-  page_stack.pop(set_weight_page)
-  page_stack.pop(set_age_page)
-  page_stack.pop(set_sex_at_birth_page)
-  page_stack.pop(set_activity_page)
-  page_stack.pop(set_plan_page)
-  page_stack.push(create_storage_page)
-}
-
 function calcCal(){
   var goal_calc = root.type_goal == "loose" ?
   (-root.user_goal) : root.type_goal == "gain" ?
